9fdc87f434169d4739964b980ce9e60904adeef4
Emerson Doyah <emerson.doyah@gmail.com>
2018-06-23 11:20:44 -0500

    add routes

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var router = require('./controllers/via_controllers');
+var db = require("./models");
+
+var app = express();
+
+// App PORT setting
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8088;
+
+app.use(router);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+// Application server.
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
