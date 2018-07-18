0b0f19c2635eb3884a216b7debf9d5505eeb65bf
Eric Baltutis <baltu008@umn.edu>
2018-07-17 20:25:24 -0500

    push to heroku

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "localhost",
+        host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
         port: 3306,
-        user: "root",
-        password: "root",
-        database: "via_db",
+        user: "r9dq67qi9v7s6ufg",
+        password: "ws5khhmguhlojjiw",
+        database: "zjxsl7amqbd9fge7",
         socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
     });
 }
