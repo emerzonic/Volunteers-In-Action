f685f78042977f5d9f6452f94b9896156057667f
Eric Baltutis <baltu008@umn.edu>
2018-07-17 20:29:04 -0500

    remove socket

--- config/connection.js
+++ config/connection.js
@@ -9,8 +9,7 @@ if (process.env.JAWSDB_URL) {
         port: 3306,
         user: "r9dq67qi9v7s6ufg",
         password: "ws5khhmguhlojjiw",
-        database: "zjxsl7amqbd9fge7",
-        socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+        database: "zjxsl7amqbd9fge7"
     });
 }
 
