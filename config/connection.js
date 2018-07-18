e0ff882ec48e9fa8f0e821404bec70403ff1502a
Eric Baltutis <baltu008@umn.edu>
2018-07-17 20:37:05 -0500

    remove jawsDB

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: "localhost",
         port: 3306,
-        user: "r9dq67qi9v7s6ufg",
-        password: "ws5khhmguhlojjiw",
-        database: "zjxsl7amqbd9fge7"
+        user: "root",
+        password: "root",
+        database: "via_db"
     });
 }
 
